c91f94578194f0e5741a1ffb7c39031eee1f4bed
yaammai <ya_amai@yahoo.co.jp>
2023-05-06 00:42:10 +0900

    move to sveltekit

--- src/Page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-import CodeMirror from './CodeMirror.svelte'
-export let params = {}
-console.log(params)
-
-</script>
-
-<CodeMirror yjsHost="ws://192.168.130.104:1234" yjsName={params.wild}/>
